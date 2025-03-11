0b8518ee909bffbc7c2fc7f312dbab1f0573ff61
Aryaman Jeendgar <jeendgararyaman@gmail.com>
2025-03-11 12:02:38 -0700

    Finished re-factoring skeleton

--- tests/examples/dnn-mode-connectivity-new.py
+++ tests/examples/dnn-mode-connectivity-new.py
@@ -11,11 +11,12 @@ import matplotlib.pyplot as plt
 from torchvision.datasets import CIFAR10
 import torchvision.transforms as transforms
 import numpy as np
+from model import resnet_164
 from torchpathdiffeq import\
     steps,\
     get_parallel_RK_solver,\
     UNIFORM_METHODS,\
-    VARIABLE_METHODS\
+    VARIABLE_METHODS
 
 
 from functools import partial
@@ -31,10 +32,13 @@ parser = argparse.ArgumentParser(
                     prog='DNN-mode-connectivity',
                     description='Constructs constant-accuracy curves between independent DNN modes',
                     epilog='Sucks to suck')
+
 parser.add_argument('-w1', '--w_init')
 parser.add_argument('-w2', '--w_end')
 parser.add_argument('-mp', '--model_path')
 parser.add_argument('-tc', '--train_curve')
+parser.add_argument('-id', '--experiment_id')
+parser.add_argument('-s', '--seed')
 
 
 normalize = transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
@@ -89,15 +93,18 @@ test_config = {
     'model_end_path': "",
     'w1_path': args.w_init,
     'w2_path': args.w_end,
-    'curve_path': "./mode_test_path_t_detached.pt"
+    'curve_path': "./mode_test_path_t_detached.pt",
+    'id': int(args.experiment_id),
+    'seed': int(args.seed)
 }
 
 test_config['optims'] = {
         'Adam': partial(optim.Adam, lr=test_config["LR"])
 }
+test_config['t_init'] = torch.tensor([0]).to(test_config['device']).type(torch.float)
+test_config['t_final'] = torch.tensor([1]).to(test_config['device']).type(torch.float)
 
-
-### Describing the network used for the path ###
+### Describing the network & Loss used for the path, BEGIN ###
 
 class CurveNet(nn.Module):
     def __init__(self, nn_dims: int, w1: torch.Tensor, w2: torch.Tensor, criterion: nn.Module):#, model: nn.Module):
@@ -117,10 +124,8 @@ class CurveNet(nn.Module):
         #TODO: How will a `CurveNet` instance actually be trained?
         coeffs = [1 - t, t, 1 - torch.cos(2 * math.pi * t)]
         # coeffs = [1 - t, t]
-        t_init = torch.tensor([0]).to(test_config['device']).type(torch.float)
-        t_final = torch.tensor([1]).to(test_config['device']).type(torch.float)
-        f1 = F.tanh(self.fc3(F.tanh(self.fc2(F.tanh(self.fc1(t_final))))))
-        f0 = F.tanh(self.fc3(F.tanh(self.fc2(F.tanh(self.fc1(t_init))))))
+        f1 = F.tanh(self.fc3(F.tanh(self.fc2(F.tanh(self.fc1(test_config['t_final']))))))
+        f0 = F.tanh(self.fc3(F.tanh(self.fc2(F.tanh(self.fc1(test_config['t_init']))))))
         t = F.tanh(self.fc1(t))
         t = F.tanh(self.fc2(t))
         t = F.tanh(self.fc3(t)) # `t` now represents a point in weight-space
@@ -189,8 +194,113 @@ class CurveNetLoss(nn.Module):
         #     loss.append(self.criterion(self.model(X), Y).view(1,1))
         # return torch.tensor(loss).to(test_config['device']).view(len(loss), 1)
 
-### Describing the network used for the path ###
+### Describing the network & Loss used for the path, END ###
 
+### Path training loop ###
+def train_path(optimizer_str: str, path: CurveNet, potential: CurveNetLoss, integrator):
+    optimizer = test_config['optims'][optimizer_str](path.parameters())
+    print(f'{optimizer_str}')
+    print(f'{test_config["device"]}')
+    int_list = []
+
+    integral = None
+
+    for epoch in range(test_config['epochs_integrator']):
+        path.train()
+        optimizer.zero_grad()
+
+        integral = integrator.integrate(
+            potential, t_init=test_config['t_init'], t_final=test_config['t_final'], max_batch=test_config['max_batches']
+        )
+
+        if test_config['max_batches'] is None:
+            integral.integral.backward()
+        int_list.append(integral.integral)
+
+        optimizer.step()
+        print(f'Epoch#: {epoch}')
+
+    # for epoch in range(test_config['epochs']):
+    #     path.train()
+    #     for i, data in enumerate(trainloader, 0):
+    #         inputs, labels = data
+    #         inputs         = inputs.to(test_config['device'])
+    #         labels         = labels.to(test_config['device'])
+
+    #         optimizer.zero_grad()
+
+
+    #         integral = integrator.integrate(
+    #             potential, t_init=t_init, t_final=t_final, max_batch=test_config['max_batches'], ode_args=(inputs, labels)
+    #         )
+
+    #         if test_config['max_batches'] is None:
+    #             integral.integral.backward()
+
+    #         optimizer.step()
+        # print(f'Epoch#: {epoch}')
+    # Saving all of the OP tensors
+    int_list = torch.tensor(int_list)
+    torch.save({
+                'epoch': test_config["epochs"],
+                'model_state_dict': path.state_dict(),
+                'optimizer_state_dict': optimizer.state_dict(),
+                }, 'mode_test_path_t_detached.pt')
+
+    torch.save(int_list, f'mode_test_integral_all_id={test_config["id"]}.pt')
+    torch.save(integral.integral, f'mode_test_integral_id={test_config["id"]}.pt')
+    torch.save(integral.loss, f'mode_test_loss_id={test_config["id"]}.pt')
+    torch.save(integral.t_pruned, f'mode_test_t_pruned_id={test_config["id"]}.pt')
+    torch.save(integral.t, f'mode_test_t_id={test_config["id"]}.pt')
+    torch.save(integral.h, f'mode_test_h_id={test_config["id"]}.pt')
+    torch.save(integral.y, f'mode_test_y_id={test_config["id"]}.pt')
+    torch.save(integral.sum_steps, f'mode_test_sum_stepid={test_config["id"]}.pt')
+    torch.save(integral.integral_error, f'mode_test_integral_erroid={test_config["id"]}.pt')
+    torch.save(integral.errors, f'mode_test_errors_id={test_config["id"]}.pt')
+    torch.save(integral.error_ratios, f'mode_test_error_ratioid={test_config["id"]}.pt')
+
+### Path training loop ###
+
+### Path eval ###
+def eval_curve(curve: CurveNet, plain_model: nn.Module, num_points: int):
+    #, tmp: bool):
+    t_values = torch.linspace(0, 1, num_points).view(num_points, 1)
+    # points = torch.lerp(w1, w2, t_values.unsqueeze(1))
+    points = curve(t_values)
+    criterion = test_config['criterion']['CE']
+    ls_list = []
+
+    # for epoch in range(test_config['epochs_integrator']):
+    print("Curve\n")
+    for i in range(num_points):
+        eval_running_loss = 0.0
+        vector_to_parameters(points[i, :], plain_model.parameters())
+        curve.eval()
+        for j, eval_data in enumerate(testloader, 0):
+            eval_X, eval_Y = eval_data
+            eval_X = eval_X.to(test_config['device'])
+            eval_Y = eval_Y.to(test_config['device'])
+            eval_outputs = plain_model(eval_X)
+
+            loss = criterion(eval_outputs, eval_Y)
+            eval_running_loss += loss.detach().item()
+
+        ls_list.append(eval_running_loss/len(testloader))
+        print(f"Point# {i}")
+
+    # if tmp:
+    #     torch.save(torch.tensor(ls_list), "resnet_w1_eval_with_state_dict.pt")
+    # else:
+    #     torch.save(torch.tensor(ls_list), "resnet_w1_eval_v2p.pt")
+
+    # return ls_list
+
+    return {
+        "loss": torch.tensor(ls_list).to(test_config['device']).view(len(ls_list), 1),
+        "t": t_values
+    }
+
+### Path eval ###
 
 ### Curve eval on a straight line, START
 
@@ -224,3 +334,42 @@ def eval_line(w1: torch.Tensor, w2: torch.Tensor, plain_model: nn.Module, num_po
     }
 
 ### Curve eval on a straight line, END
+
+def setup(seed: int):
+    """
+    Sets up everything for training/eval
+    """""
+    parallel_integrator = get_parallel_RK_solver(
+        test_config['sampling_type'], method=test_config['method'], atol=test_config['atol'],\
+        rtol=test_config['rtol'], remove_cut=0.1, dtype=torch.float
+    ) # instantiating the integrator
+
+    plain_model = resnet_164(10, seed)
+
+    model_w1 = torch.load(test_config['w1_path'], map_location=test_config['device']) # we have `epoch`, `model_state_dict` and..
+    model_w2 = torch.load(test_config['w2_path'], map_location=test_config['device']) # ... `optimizer_state_dict` information
+
+    # names of subset of parameters to grab from state_dict
+    param_names = {name for name, _ in plain_model.named_parameters()}
+
+    model_w1_params = [param for name, param in model_w1['model_state_dict'].items() if name in param_names]
+    w1 = parameters_to_vector(model_w1_params) # flattened params
+
+    model_w2_params = [param for name, param in model_w2['model_state_dict'].items() if name in param_names]
+    w2 = parameters_to_vector(model_w2_params) # flattened params
+
+    return (parallel_integrator, plain_model, w1, w2)
+
+
+if __name__ == '__main__':
+    train = True
+
+    parallel_integrator, plain_model, w1, w2 = setup(test_config['seed'])
+    if train:
+        path = CurveNet(w2.shape[0], w1, w2, test_config['criterion']['CE']).to(test_config['device']) # path NN
+        path_loss = CurveNetLoss(test_config['criterion']['CE'], plain_model, path).to(test_config['device'])# potential along path
+        train_path('Adam', path, path_loss, parallel_integrator)
+    else:
+        #TODO: re-write evaluation routine
+        pass
+    pass
