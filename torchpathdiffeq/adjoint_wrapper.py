d4c6edeb2707cb44e04991351257ac23efa86ba8
khegazy <KareemHegz@gmail.com>
2025-06-10 16:51:45 -0700

    initial commit

---
+++ torchpathdiffeq/adjoint_wrapper.py
@@ -0,0 +1,13 @@
+import torch
+from torch import nn
+
+class AdjointClass(torch.autograd.Function):
+    def forward():
+    
+    def backward():
+    
+    def adjoint_augmented():
+    
+    def setup_adjoint():
+        raise NotImplementedError
+        
