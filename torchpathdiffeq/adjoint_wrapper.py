ae847ae03264c91004e77d6b2a698b12af597ef5
khegazy <KareemHegz@gmail.com>
2025-06-10 17:16:33 -0700

    outline of initial adjoin function

--- torchpathdiffeq/adjoint_wrapper.py
+++ torchpathdiffeq/adjoint_wrapper.py
@@ -10,4 +10,8 @@ class AdjointClass(torch.autograd.Function):
     
     def setup_adjoint():
         raise NotImplementedError
-        
+
+def basic_usage_fxn(method_name, fxn, t_init=0, t_final=1):
+    integrator = get_parallel_RK_solver(method_name, fxn, t_init=t_init, t_final=t_final)
+    ans = AdjointClass.apply(integrator)
+
