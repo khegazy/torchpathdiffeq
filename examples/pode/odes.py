1d4b9690dfd0b212a3048d513909ff1777a5c0bf
khegazy <KareemHegz@gmail.com>
2025-09-20 14:39:45 -0700

    library of odes

---
+++ examples/pode/odes.py
@@ -0,0 +1,22 @@
+import torch
+
+class BaseODE():
+    def __init__(self, N_dims):
+        self.N_dims = N_dims
+    
+    def __call__(self, t):
+        raise NotImplementedError
+
+class linear(BaseODE):
+    def __init__(self):
+        super().__init__(1)
+
+    def __call__(self, t):
+        return t
+
+class quadratic(BaseODE):
+    def __init__(self):
+        super().__init__(1)
+    
+    def __call__(self, t):
+        return t**2
