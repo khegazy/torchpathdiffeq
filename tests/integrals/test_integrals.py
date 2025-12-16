c01c911f60166f2199ccbb36cd32ebfa7331930e
khegazy <KareemHegz@gmail.com>
2025-12-16 10:19:22 +0200

    change eval functions to classes

--- tests/integrals/test_integrals.py
+++ tests/integrals/test_integrals.py
@@ -3,16 +3,24 @@ import torch
 from torchpathdiffeq import\
     steps,\
     get_parallel_RK_solver,\
+    examples,\
     SerialAdaptiveStepsizeSolver,\
     ode_path_integral,\
     UNIFORM_METHODS,\
-    VARIABLE_METHODS,\
-    ODE_dict
+    VARIABLE_METHODS
 
+ode_evals = [
+    ("Unity", [()]),
+    ("Identity", [()]),
+    ("Squared", [()]),
+    ("SineSquared", [(1,), (10,), (50,)]),
+    ("Exponential", [(0.1,), (1,), (5,)]),
+    ("DampedSine", [(1, 0.1), (1, 5), (50, 0.1), (50, 5)])
+]
 
 def test_integrals():
-    atol = 1e-10
-    rtol = 1e-8
+    atol = 1e-11
+    rtol = 1e-9
     t_init = torch.tensor([0], dtype=torch.float64)
     t_final = torch.tensor([1], dtype=torch.float64)
     #loop_items = zip(
@@ -33,50 +41,52 @@ def test_integrals():
                 cutoff = 1e-6
             #if method != 'fehlberg2':
             #    continue
-            for name, (ode, solution) in ODE_dict.items():
-                print("INTEGRAL", method, name, sampling_name)
-                correct = solution(t_init=t_init, t_final=t_final)
-                parallel_integrator = get_parallel_RK_solver(
-                    sampling_type, method=method, atol=atol, rtol=rtol, remove_cut=0.1
-                )
-                integral_output = parallel_integrator.integrate(
-                    ode, t_init=t_init, t_final=t_final
-                )
-                #cutoff = 10000*10**(-1*parallel_integrator.order)
-                
-                error_string = f"{sampling_name} {method} failed to properly integrate {name}, calculated {integral_output.integral.item()} but expected {correct.item()}"
-                t_flat = torch.flatten(integral_output.t[:,:,0])
-                t_flat_unique = torch.flatten(integral_output.t[:,1:,0])
-                """
-                serial_integral = ode_path_integral(
-                    ode_fxn=ode,
-                    method=method,
-                    computation='serial',
-                    atol=atol,
-                    rtol=rtol,
-                    y0=torch.tensor([0], dtype=torch.float64),
-                    t=None,
-                )
-                print("SERIAL", serial_integral.integral, torch.abs((serial_integral.integral - correct)/correct))
-                """
-                assert torch.abs((integral_output.integral.cpu() - correct)/correct) < cutoff, error_string
+            for name, ode_params in ode_evals:
+                for params in ode_params:
+                    print("INTEGRAL", method, name, sampling_name, params)
+                    ode = getattr(examples, name)(*params)
+                    parallel_integrator = get_parallel_RK_solver(
+                        sampling_type, method=method, atol=atol, rtol=rtol, remove_cut=0.1
+                    )
+                    integral_output = parallel_integrator.integrate(
+                        ode, t_init=t_init, t_final=t_final
+                    )
+                    #cutoff = 10000*10**(-1*parallel_integrator.order)
+                    
+                    t_flat = torch.flatten(integral_output.t[:,:,0])
+                    t_flat_unique = torch.flatten(integral_output.t[:,1:,0])
+                    """
+                    serial_integral = ode_path_integral(
+                        ode_fxn=ode,
+                        method=method,
+                        computation='serial',
+                        atol=atol,
+                        rtol=rtol,
+                        y0=torch.tensor([0], dtype=torch.float64),
+                        t=None,
+                    )
+                    print("SERIAL", serial_integral.integral, torch.abs((serial_integral.integral - solution)/solution))
+                    """
+                    solution = ode.solve(t_init=t_init, t_final=t_final)
+                    error_string = f"{sampling_name} {method} failed to properly integrate {name}, calculated {integral_output.integral.item()} but expected {solution.item()}"
+                    assert torch.abs((integral_output.integral.cpu() - solution)/solution) < cutoff, error_string
 
-                t_flat = torch.flatten(integral_output.t, start_dim=0, end_dim=1)
-                t_optimal_flat = torch.flatten(integral_output.t_optimal, start_dim=0, end_dim=1)
-                assert torch.all(t_flat[1:] - t_flat[0:-1] >= 0)
-                assert torch.all(t_optimal_flat[1:] - t_optimal_flat[0:-1] >= 0)
-                assert torch.allclose(integral_output.t[1:,0,:], integral_output.t[:-1,-1,:])
-                
-                """
-                if max_batch is None:
-                    no_batch_integral = integral_output
-                    no_batch_delta = torch.abs(no_batch_integral.integral - correct)
-                else:
-                    rel_tol = 1e-3
-                    if torch.abs(integral_output.integral - correct) > no_batch_delta:
-                        print(no_batch_integral.integral, integral_output.integral)
-                        assert torch.abs(1 - (no_batch_integral.integral/integral_output.integral)) < rel_tol
-                        assert 10*torch.abs(no_batch_integral.integral_error) >= torch.abs(integral_output.integral_error)
-                    assert len(no_batch_integral.t) <= len(integral_output.t)
-                    assert len(no_batch_integral.t_optimal) <= len(integral_output.t_optimal) 
-                """
+                    t_flat = torch.flatten(integral_output.t, start_dim=0, end_dim=1)
+                    t_optimal_flat = torch.flatten(integral_output.t_optimal, start_dim=0, end_dim=1)
+                    assert torch.all(t_flat[1:] - t_flat[0:-1] >= 0)
+                    assert torch.all(t_optimal_flat[1:] - t_optimal_flat[0:-1] >= 0)
+                    assert torch.allclose(integral_output.t[1:,0,:], integral_output.t[:-1,-1,:])
+                    
+                    """
+                    if max_batch is None:
+                        no_batch_integral = integral_output
+                        no_batch_delta = torch.abs(no_batch_integral.integral - solution)
+                    else:
+                        rel_tol = 1e-3
+                        if torch.abs(integral_output.integral - solution) > no_batch_delta:
+                            print(no_batch_integral.integral, integral_output.integral)
+                            assert torch.abs(1 - (no_batch_integral.integral/integral_output.integral)) < rel_tol
+                            assert 10*torch.abs(no_batch_integral.integral_error) >= torch.abs(integral_output.integral_error)
+                        assert len(no_batch_integral.t) <= len(integral_output.t)
+                        assert len(no_batch_integral.t_optimal) <= len(integral_output.t_optimal) 
+                    """
--- torchpathdiffeq/examples.py
+++ torchpathdiffeq/examples.py
@@ -1,53 +1,79 @@
 import torch
 
-def identity(t, y=None):
-    return 1
+class Unity():
+    __name__ = "Unity"
+    def __init__(self) -> None:
+        pass
+    
+    def __call__(self, t, y=None):
+        print("T", t.shape)
+        return torch.ones_like(t, device=t.device, dtype=t.dtype)
+    
+    def solve(self, t_init, t_final):
+        return t_final - t_init
 
-def identity_solution(t_init, t_final):
-    return t_final - t_init
+class Identity():
+    __name__ = "Identity"
+    def __init__(self) -> None:
+        pass
 
-def t(t, y=None):
-    return t
+    def __call__(self, t, y=None):
+        return t
 
-def t_solution(t_init, t_final):
-    return 0.5*(t_final**2 - t_init**2)
+    def solve(self, t_init, t_final):
+        return 0.5*(t_final**2 - t_init**2)
 
-def t_squared(t, y=None):
-    return t**2
+class Squared:
+    __name__ = "Squared"
+    def __init__(self) -> None:
+        pass
+        
+    def __call__(self, t, y=None):
+        return t**2
 
-def t_squared_solution(t_init, t_final):
-    return (t_final**3 - t_init**3)/3.
+    def solve(self, t_init, t_final):
+        return (t_final**3 - t_init**3)/3.
 
-def sine_squared(t, w=3.7, y=None):
-    return torch.sin(t*w*2*torch.pi)**2
+class SineSquared:
+    __name__ = "SineSquared"
+    def __init__(self, freqency) -> None:
+        self.w = freqency
+        self.w_sol = 2*self.w
+    
+    def __call__(self, t, y=None):
+        return torch.sin(t*self.w)**2
 
-def sine_squared_solution(t_init, t_final, w=3.7):
-    _w = 4*torch.pi*w
-    return (t_final - t_init)/2.\
-        - (torch.sin(torch.tensor([_w*t_final])) - torch.sin(torch.tensor([_w*t_init])))/(2*_w)
+    def solve(self, t_init, t_final):
+        solution = torch.sin(torch.tensor([self.w_sol*t_init]))
+        solution -= torch.sin(torch.tensor([self.w_sol*t_final]))
+        return (t_final - t_init)/2. + solution/(2*self.w_sol)
 
-def exp(t, a=5, y=None):
-    return torch.exp(a*t)
+class Exponential:
+    __name__ = "Exponential"
+    def __init__(self, decay_constant) -> None:
+        self.t_const = decay_constant
+    
+    def __call__(self, t, y=None):
+        return torch.exp(self.t_const*t)
 
-def exp_solution(t_init, t_final, a=5):
-    return (torch.exp(torch.tensor([t_final*a]))\
-        - torch.exp(torch.tensor([t_init*a])))/a
+    def solve(self, t_init, t_final):
+        return (torch.exp(torch.tensor([t_final*self.t_const]))\
+            - torch.exp(torch.tensor([t_init*self.t_const])))/self.t_const
 
-def damped_sine(t, w=3.7, a=5, y=None):
-    return torch.exp(-a*t)*torch.sin(w*t*2*torch.pi)
+class DampedSine:
+    __name__ = "DampedSine"
+    def __init__(self, freqency, decay_constant):
+        assert decay_constant > 0
+        self.w = freqency
+        self.t_const = decay_constant
 
-def damped_sine_solution(t_init, t_final, w=3.7, a=5):
-    _w = 2*torch.pi*w
-    def numerator(t, w, a):
+    def __call__(self, t, y=None):
+        return torch.exp(-self.t_const*t)*torch.sin(self.w*t)
+
+    def _numerator(self, t):
         t = torch.tensor([t])
-        return -1*torch.exp(-a*t)*(a*torch.sin(_w*t) + _w*torch.cos(_w*t))
-    return (numerator(t_final, w, a) - numerator(t_init, w, a))/(a**2 + _w**2)
-
-
-ODE_dict = {
-    "t" : (t, t_solution),
-    "t_squared" : (t_squared, t_squared_solution),
-    "sine_squared" : (sine_squared, sine_squared_solution),
-    "exp" : (exp, exp_solution),
-    "damped_sine" : (damped_sine, damped_sine_solution)
-}
+        return -1*torch.exp(-self.t_const*t)*(self.t_const*torch.sin(self.w*t) + self.w*torch.cos(self.w*t))
+    
+    def solve(self, t_init, t_final):
+        denomenator = self.t_const**2 + self.w**2
+        return (self._numerator(t_final) - self._numerator(t_init))/denomenator
